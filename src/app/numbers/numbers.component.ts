c89c25ae9ca9102dd72d01fb81bd960c6e5f54ad
Sergey Koshechkin <serge.koshechkin@gmail.com>
2017-05-24 06:14:59 +0300

    css -> scss

--- src/app/numbers/numbers.component.ts
+++ src/app/numbers/numbers.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-numbers',
   templateUrl: './numbers.component.html',
-  styleUrls: ['./numbers.component.css']
+  styleUrls: ['./numbers.component.scss']
 })
 export class NumbersComponent implements OnInit {
 
