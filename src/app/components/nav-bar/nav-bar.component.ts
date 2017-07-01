e5d6a5acce2f40999a1276a0d37b4b3713dc6b7f
Sergey Koshechkin <serge.koshechkin@gmail.com>
2017-07-01 14:51:49 +0300

    (NavBarComponent) feat: implement logout and login states

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { UserService } from '../../shared/models/user/user.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
-  ngOnInit() {
+  isLoggedIn(): boolean {
+    return this.userService.isLoggedIn();
   }
 
+  logout() {
+    this.userService.logout()
+  }
 }
