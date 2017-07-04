a327fbbec9113610d657f851da4e3efb75614cd8
Sergey Koshechkin <serge.koshechkin@gmail.com>
2017-07-04 23:15:08 +0300

    (UserService) chore: change urls + huge refactor

--- src/app/shared/models/user/user.service.ts
+++ src/app/shared/models/user/user.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http';
-import { environment } from '../../../../environments/environment';
-import 'rxjs/add/operator/catch';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import { environment } from '../../../../environments/environment';
 
 @Injectable()
 export class UserService {
-  private loggedIn = false;
-  private authUrl = `https://cleanfootprint.org/accounts`;
+  private loggedIn = new BehaviorSubject(false);
+  private authUrl = `${environment.apiUrl}user/`;
   private requestOptions = new RequestOptions({
     headers: new Headers({
       'Content-Type': 'application/json',
@@ -15,19 +18,25 @@ export class UserService {
     }),
     withCredentials: true,
   });
+
   constructor(private http: Http) {
-    this.getState().subscribe(status => this.loggedIn = status);
+    this.updateState();
   }
 
-  getState(): Observable<boolean> {
-    return this.http.get(`${environment.apiUrl}user/get_state/`)
+  updateState(): Subscription {
+    return this.http.get(`${this.authUrl}get_state/`)
       .map(res => res.json() as { is_authenticated: boolean })
-      .map(res => res.is_authenticated)
+      .map(res => {
+        console.log(res);
+        return res.is_authenticated;
+      })
+      // .catch(errRes => errRes.json())
+      .subscribe(status => this.loggedIn.next(status));
   }
 
   login(email, password): Observable<any> {
-    const url = `${this.authUrl}/login/`;
-    const body = JSON.stringify({ email, password });
+    const url = `${this.authUrl}login/`;
+    const body = JSON.stringify({email, password});
     const options = this.requestOptions;
     return this.http.post(url, body, options)
       .map(res => res.json())
@@ -39,7 +48,7 @@ export class UserService {
   }
 
   logout(): Observable<boolean> {
-    const url = `${this.authUrl}/logout/`;
+    const url = `${this.authUrl}logout/`;
     const options = this.requestOptions;
     return this.http.post(url, null, options)
       .map(res => res.json())
@@ -50,27 +59,39 @@ export class UserService {
   }
 
   isLoggedIn(): boolean {
-    return this.loggedIn;
+    return this.loggedIn.getValue();
   }
 
-  signUp(name, email, password1): Observable<boolean | string> {
-    const url = `${this.authUrl}/signup/`;
-    // const body = JSON.stringify({ email, password1 });
-    const body = { email, password1 };
+  signUp(email, password): Observable<boolean | string> {
+    const url = `${this.authUrl}sign_up/`;
+    const body = {email, password};
     const options = this.requestOptions;
     return this.http.post(url, body, options)
       .map(res => res.json())
       .map((res) => {
-        console.log('signup', res);
+
         if (res.success) {
-          // this.loggedIn = true;
+          this.updateState();
         }
 
         return res.success;
       })
-      .catch(err => {
-        console.error(err);
-        return err;
-      });
+      .catch(err => this.handleError(err));
+  }
+  private extractData(res: Response | any) {
+
+  }
+  private handleError(error: Response | any): string | any {
+    // In a real world app, you might use a remote logging infrastructure
+    console.warn('raw', error);
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      console.log('(UserService) handleError:', body);
+      return Observable.throw(body);
+    } else {
+      const errMsg = error.message ? error.message : error.toString();
+      console.log('(UserService) handleError:', errMsg);
+      return Observable.throw(errMsg);
+    }
   }
 }
