aae9e3a09052818bc8423b73132d7f7e43f1d021
Sergey Koshechkin <serge.koshechkin@gmail.com>
2017-07-06 04:35:50 +0300

    (Footer) fix: nav

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from '../../shared/models/user/user.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
+export class FooterComponent implements OnInit, OnDestroy {
+  sub: Subscription;
+  loggedIn = false;
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.sub = this.userService.isLoggedIn()
+      .subscribe(status => this.loggedIn = status);
+  }
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+  logout() {
+    this.userService.logout().subscribe(_ => this.loggedIn = false);
   }
 
 }
